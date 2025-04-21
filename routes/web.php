2b36ab58ed8c3733889061414df44078fe700102
Ripon Shikder <coderripon@gmail.com>
2025-04-21 23:41:38 +0600

    function

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
-    return view('welcome');
+    //$my_array = [100, 2, 3, 4, "", "Ripon"];
+    $my_array = ["karim", "rahim", 3, 4, "", "Ripon"];
+    $gm = ["ripon@hotmail.com", "karim@gmail.com"];
+
+    $new_array = array_map(
+        fn($value) => $value . " - verified",
+        array_filter($gm, fn($value) => str_ends_with($value, "@gmail.com"))
+    );
+    return $new_array;
+
+
+    // $numbers = [1, 2, 3, 4, 5, 6];
+
+    // $evenSquares = array_map(function($number) {
+    //     return $number * $number;
+    // }, array_filter($numbers, function($number) {
+    //     return $number % 2 === 0;
+    // }));
+
+    // return $evenSquares;
 });
